70044943ee713e736d79add0dfde8c82203f8e11
Matthew A. Terry <mat@timepiecestudios.net>
2010-12-09 02:55:25 -0500

    Removed cmap.c and cmap.h, and instead replaced them with crel.c and crel.h, the source and the header files for the relation structure definitions and handling. Parsing will be worked on later.

---
+++ include/common/crel.h
@@ -0,0 +1,50 @@
+#ifndef _C_REL_
+#define _C_REL_
+//  crel.h
+//  Relational data structure and functions for manipulating relations in C
+//  Matthew A. Terry (terrym@uwindsor.ca)
+//  ===========================================================================
+//  include statements
+#include <string.h>
+#include <stdlib.h>
+
+//  Implementation notes:
+//    Relations are organized into tables, which have a number of elements:
+//      - table name (string)
+//      - data storage location (string)
+//      - attribute definitions
+//        - attribute name (string)
+//        - attribute length (int)
+//        - attribute constraints
+//          - primary key
+
+//  attribute constraints structure definition:
+struct crel_attr_constraints
+{
+  unsigned int primary_key  : 1;
+};
+typedef struct crel_attr_constraints crel_attr_constraints_t;
+
+//  attribute list structure definition:
+struct crel_attr
+{
+  char *name;
+  int length;
+  crel_attr_constraints_t* constraints;
+};
+typedef struct crel_attr crel_attr_t;
+
+//  relation table structure definition
+struct crel
+{
+  char *name;
+  char *file_loc;
+  crel_attr_t *attr_list;
+};
+typedef struct crel crel_t;
+
+//  function declarations
+crel_t new_relation();
+
+
+#endif
---
+++ src/common/crel.c
@@ -0,0 +1,6 @@
+// crel.c
+// General relation class
+// Matthew A. Terry (terrym@uwindsor.ca)
+// A general relational data structure and functions for accessing and modifying
+// the relation.
+// ----------------------------------------------------------------------------
