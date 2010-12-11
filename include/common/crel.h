6dc4fc65bddf8dcaddd216eca14d1e7ad5ac21bb
Matthew A. Terry <mat@timepiecestudios.net>
2010-12-10 22:08:19 -0500

    Added more functions to crel.h

--- include/common/crel.h
+++ include/common/crel.h
@@ -23,30 +23,44 @@ struct crel_attr_constraints
 {
   unsigned int primary_key  : 1;
 };
-typedef struct crel_attr_constraints crel_attr_constraints_t;
+typedef struct crel_attr_constraints * crel_attr_constraints_t;
 
 //  attribute list structure definition:
 struct crel_attr
 {
+  int index;
   char *name;
   int length;
-  crel_attr_constraints_t* constraints;
+  crel_attr_constraints_t constraints;
 };
-typedef struct crel_attr crel_attr_t;
+typedef struct crel_attr * crel_attr_t;
 
 //  relation table structure definition
 struct crel
 {
   char *name;
   char *file_loc;
-  crel_attr_t *attr_list;
+  int next_order_available;
+  crel_attr_t attr_list;
 };
-typedef struct crel crel_t;
+typedef struct crel * crel_t;
 
 //  function declarations
-//  creating a new relational table
+//  creating a new relational table; returns the new table
 crel_t new_reltable();
+//  delete a relational table; returns the deleted table with all other
+//  references removed
+crel_t del_reltable();
 
+//  add attribute to a table; returns 0 for successful
+int add_attribute(crel_t, crel_attr_t);
+//  remove attribute from a table; returns a pointer to the removed attribute
+crel_attr_t rem_attribute(crel_t, crel_attr_t);
+//  change attribute name; returns a pointer to the changed attribute
+crel_attr_t ch_attribute(crel_attr_t, char *);
+//  find an attribute by name in a table
+crel_attr_t find_attribute(crel_t, char *);
 
-
+//  set an attribute to be a primary key; returns
+int
 #endif
